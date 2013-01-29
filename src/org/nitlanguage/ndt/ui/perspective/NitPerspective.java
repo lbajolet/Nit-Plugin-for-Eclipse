67162f870ed6a2d2679752b80b7fa50a8f37a680
heu.nathan <heu.nathan@courrier.uqam.ca>
2013-01-29 14:00:00 +0100

    Hard coded Views Ids (e.g. "org.eclipse.ui.console.ConsoleView") partially replaced with plateform constants. Minor syntax and name changes made

--- src/org/nitlanguage/ndt/ui/perspective/NitPerspective.java
+++ src/org/nitlanguage/ndt/ui/perspective/NitPerspective.java
@@ -1,8 +1,9 @@
 package org.nitlanguage.ndt.ui.perspective;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
-import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.console.IConsoleConstants;
 
 /**
  * The nit Perspective - Defines initial page layout 
@@ -18,18 +19,20 @@ public class NitPerspective implements IPerspectiveFactory {
 		addFastViews(layout);
 		addViewShortcuts(layout);
 		addPerspectiveShortcuts(layout);
-		{
-			IFolderLayout folderLayout = layout.createFolder("folder", IPageLayout.LEFT, 0.32f, IPageLayout.ID_EDITOR_AREA);
-			folderLayout.addView("org.eclipse.ui.navigator.ProjectExplorer");
-			folderLayout.addView("org.eclipse.ui.views.ContentOutline");
-		}
-		{
-			IFolderLayout folderLayout = layout.createFolder("folder_1", IPageLayout.BOTTOM, 0.79f, IPageLayout.ID_EDITOR_AREA);
-			folderLayout.addView("org.eclipse.pde.runtime.LogView");
-			folderLayout.addView("org.eclipse.ui.views.ProgressView");
-			folderLayout.addView("org.eclipse.ui.views.AllMarkersView");
-			folderLayout.addView("org.eclipse.ui.console.ConsoleView");
-		}
+		
+		IFolderLayout leftFolder = layout.createFolder("left", IPageLayout.LEFT, 0.32f, editorArea);
+		leftFolder.addView(IPageLayout.ID_PROJECT_EXPLORER);
+		//folderLayout.addView("org.eclipse.ui.views.ContentOutline");
+
+		IFolderLayout bottomFolder = layout.createFolder("bottom", IPageLayout.BOTTOM, 0.79f, editorArea);
+		bottomFolder.addView(IConsoleConstants.ID_CONSOLE_VIEW);
+		bottomFolder.addView("org.eclipse.pde.runtime.LogView");
+		bottomFolder.addView(IPageLayout.ID_PROGRESS_VIEW);
+		bottomFolder.addView("org.eclipse.ui.views.AllMarkersView");
+
+		IFolderLayout folderLayout = layout.createFolder("right", IPageLayout.RIGHT, 0.6f, editorArea);
+		folderLayout.addView(IPageLayout.ID_OUTLINE);
+
 	}
 
 	/**
